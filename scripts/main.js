62eaed5de5a886ba66af523ea8ccccbce36d2058
luna215 <paulluna0215@gmail.com>
2019-06-06 22:07:24 -0700

    improved scroling/ added lodash/added more animations

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+var currentPage = 'homepage';
+
+function handleScroll(e) {
+    var delta = e.originalEvent.detail;
+        
+        if (delta < 0) {
+            if(currentPage === 'projects') {
+                currentPage = 'homepage';
+                $('#projects').removeClass('active');
+                $('#homepage').addClass('active');
+            } else if(currentPage === 'contact') {
+                currentPage = 'projects';
+                $('#contact').removeClass('active');
+                $('#projects').addClass('active');
+            }
+        } else if (delta > 0) {
+            if(currentPage === 'homepage') {
+                // TODO: Need to pause program here to not keep it from scrolling
+                currentPage = 'projects';
+                $('#homepage').removeClass('active');
+                $('#projects').addClass('active');
+                $('.sub-content').addClass('slide-north ');
+                $('.title').addClass('slide-northeast');
+                $('.start-project-button').addClass('slide-north');
+                $('.scroll-link').addClass('slide-east');
+                
+            } else if(currentPage === 'projects') {
+                currentPage = 'contact'
+                $('#projects').removeClass('active');
+                $('#contact').addClass('active');
+            } else {
+                console.warn(`Currently on ${currentPage}`);
+            }
+        }
+}
+
+$(document).ready(function() {
+    // $('html').on('DOMMouseScroll', function (e) {
+    //     _.throttle(handleScroll(e), 1000);
+    // });
+    $('html').on('DOMMouseScroll', _.throttle(handleScroll, 2050));
+});
