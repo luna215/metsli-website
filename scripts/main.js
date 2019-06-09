3a6f42bbc3e4e9c7f5a28053c9c8ad1598323a66
luna215 <paulluna0215@gmail.com>
2019-06-08 21:36:48 -0700

    added UI touces to clients page

--- scripts/main.js
+++ scripts/main.js
@@ -8,14 +8,31 @@ function handleScroll(e) {
                 currentPage = 'homepage';
                 $('#projects').removeClass('active');
                 $('#homepage').addClass('active');
+                $('.projectspage').addClass('slide-south');
+                $('.title').removeClass('slide-northeast');
+                $('.sub-content').removeClass('slide-north');
+                $('.start-project-button').removeClass('slide-north');
+                $('.scroll-link').removeClass('slide-east');
+                $('.portfolio').removeClass('slide-east-in');
+                $('.preview-forest').removeClass('slide-west-in');
+                $('.preview-tacos').removeClass('slide-west-in');
+                $('.preview-bemi').removeClass('slide-west-in');
+                $('.overlay-forest').removeClass('slide-west-in');
+                $('.projectspage').removeClass('slide-south');
             } else if(currentPage === 'contact') {
                 currentPage = 'projects';
                 $('#contact').removeClass('active');
                 $('#projects').addClass('active');
+                $('.portfolio').addClass('slide-east-in');
+                $('.preview-forest').addClass('slide-west-in');
+                $('.preview-tacos').addClass('slide-west-in');
+                $('.preview-bemi').addClass('slide-west-in');
+                $('.overlay').addClass('slide-west-in');
+            } else {
+                console.warn(`Currently on page: ${currentPage}`);
             }
         } else if (delta > 0) {
             if(currentPage === 'homepage') {
-                // TODO: Need to pause program here to not keep it from scrolling
                 currentPage = 'projects';
                 $('#homepage').removeClass('active');
                 $('#projects').addClass('active');
@@ -24,14 +41,22 @@ function handleScroll(e) {
                 $('.start-project-button').addClass('slide-north');
                 $('.scroll-link').addClass('slide-east');
                 $('.portfolio').addClass('slide-east-in');
-                $('.preview').addClass('slide-west-in');
-                $('.overlay').addClass('slide-west-in');
+                $('.preview-forest').addClass('slide-west-in');
+                $('.preview-tacos').addClass('slide-west-in');
+                $('.preview-bemi').addClass('slide-west-in');
+                $('.overlay-forest').addClass('slide-west-in');
+                $('.view-more-button').addClass('slide-east-in');
                 
             } else if(currentPage === 'projects') {
                 currentPage = 'contact'
                 $('#projects').removeClass('active');
                 $('#contact').addClass('active');
-                $('.projectspage').addClass('slide-north');
+                $('.portfolio').removeClass('slide-east-in');
+                $('.portfolio').removeClass('slide-northeast');
+                $('.preview-forest').removeClass('slide-west-in');
+                $('.preview-tacos').removeClass('slide-west-in');
+                $('.preview-bemi').removeClass('slide-west-in');
+                $('.overlay-forest').removeClass('slide-west-in');
             } else {
                 console.warn(`Currently on ${currentPage}`);
             }
